13ba317e0d1b057fade8e2113ed2628ec3362c7b
adam <adam.lenez@student.ehb.be>
2024-12-03 14:41:25 +0100

    Updates

--- _nuxt/D-ynG2fA.js
+++
@@ -1,1 +0,0 @@
-import{v as t,x as o,t as a}from"./B_f75T8k.js";const l={__name:"index",setup(r){return(n,e)=>(a(),t("div",null,e[0]||(e[0]=[o("h1",null,"Welcome to the Dashboard",-1)])))}};export{l as default};
